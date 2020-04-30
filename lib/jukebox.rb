99fc1119d4f7d5d66275dd4584c86883995e2792
Bashir <bashiralhanshali@yahoo.com>
2020-04-30 16:26:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -55,9 +55,18 @@ end
 
 def run 
   puts "Please enter a command:"
-  user_input = gets.strip 
+  user_input = "none"
   while user_input != "exit" do 
-   
+    user_input = gets.strip 
+    if user_input == "help"
+      help
+    elsif user_input == "play" 
+      play(songs) 
+    elsif user_input == "list"
+      list(songs) 
+    elsif user_input == "exit"
+      exit_jukebox
+    end 
   end 
   
 end 
