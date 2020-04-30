9859bfc743c12b453c7d15bbf28b0948a4f5c6f4
Bashir <bashiralhanshali@yahoo.com>
2020-04-30 15:59:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,7 +14,8 @@ songs = [
 def help 
   puts "I accept the following commands:"
   puts "- help : displays this help message"
-  puts ""
+  puts "- list : displays a list of songs you can play"
+  puts 
 end 
 
 
